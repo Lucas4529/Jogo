ca72aa776ef381a5e4840101805d27e6b135ca4f
Lucas4529 <133007269+Lucas4529@users.noreply.github.com>
2023-08-24 16:22:13 -0300

    Adicionei o arquivo js

---
+++ script.js
@@ -0,0 +1,27 @@
+function soma(){
+    let n1 = Number(prompt("coloque o primeiro numero: "))
+    let n2 = Number(prompt("coloque o segundo numero: "))
+    let soma = n1 + n2
+    alert("O resultado é"+soma)
+}
+function subtração(){
+    let n1 = Number(prompt("coloque o primeiro numero: "))
+    let n2 = Number(prompt("coloque o segundo numero: "))
+    let subtração = n1 - n2
+    alert("O resultado é"+subtração)
+}
+function multiplicação(){
+    let n1 = Number(prompt("coloque o primeiro numero: "))
+    let n2 = Number(prompt("coloque o segundo numero: "))
+    let multiplicação = n1 * n2
+    alert("O resultado é"+multiplicação)
+}
+function divisão(){
+    let n1 = Number(prompt("coloque o primeiro numero: "))
+    let n2 = Number(prompt("coloque o segundo numero: "))
+    let divisão = n1 / n2
+    alert("O resultado é"+divisão)
+}
+
+
+
